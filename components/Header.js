6dc573baf5001ff7f7b9d91c5a36c4cffbcf887b
Shreedhar Raj <shreedharraj.buisness@gmail.com>
2021-12-18 20:19:34 +0530

    minorchange

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,7 @@ function Header() {
     return (
         <div className="sticky top-0 p-5 z-50 shadow-sm border-b-2 border-sky-700 bg-black">
             <div className="gird gird-cols-5 lg:grid-cols-6 items-end lg:items-center">
-                <div className="flex max-w-[7%] rounded-lg p-1 bg-sky-500 rounded-lg overflow-hidden font-bold hover:bg-pink-400 col-span-4">
+                <div className="flex max-w-[7%] rounded-lg p-1 bg-sky-500 rounded-lg overflow-hidden font-bold hover:bg-sky-400 col-span-4">
                 <LoginIcon className="h-7 w-7 mr-1" />
                 <button onClick={logout} className="">Logout</button>
                 </div>
